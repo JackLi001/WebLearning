96ee3b824edb7d7298ac44cb6d065ed9263dd004
jackLi001 <3065967840@qq.com>
2024-07-10 16:42:53 +0800

    第五次提交(7.10) 更改了RealTime.js文件,添加了评论功能。 new file:   Comment.html new file:   js/Comment.js modified:   js/RealTime.js modified:   test/RealTime.html

---
+++ js/Comment.js
@@ -0,0 +1,62 @@
+        
+ function addComment(){
+
+    //var person = document.getElementById("commentPerson").value;
+    
+    //获取输入框的值,createTextNode()方法用于创建文本节点
+    var person = document.createTextNode(commentForm.commentPerson.value);
+    var content = document.createTextNode(commentForm.commentContent.value);
+    var time = document.createTextNode(updateTime_2());//获取当前时间
+
+    //创建td元素,createElement()方法用于创建元素节点
+    var td_person = document.createElement("td");
+    var td_content = document.createElement("td");
+    var td_time = document.createElement("td");
+    var tr = document.createElement("tr");
+
+    //获取tbody元素，getElementsByTagName()方法用于获取指定标签名的元素列表
+    var tbody = document.getElementsByTagName("tbody")[0];
+
+    //向td元素中添加内容，appendChild()方法可以添加一个节点或节点列表到另一个节点的末尾
+    td_person.appendChild(person);
+    td_content.appendChild(content);
+    td_time.appendChild(time);
+
+    tr.appendChild(td_person);
+    tr.appendChild(td_content);
+    tr.appendChild(td_time);
+
+    tbody.appendChild(tr);
+
+    var tComment = document.getElementById("commentTable");
+    tComment.appendChild(tbody);
+
+    commentForm.commentPerson.value = "";
+    commentForm.commentContent.value = "";
+
+}
+
+function refreshComment(){
+    location.reload();//刷新页面,reload()方法用于重新加载当前页面
+}
+
+function deleteComment(){
+    var tComment = document.getElementById("commentTable");
+    var tbody = tComment.getElementsByTagName("tbody")[0];
+    if(tbody.rows.length > 0){
+        tbody.deleteRow(tbody.rows.length-1);//删除最后一行,deleteRow()方法用于删除指定行,参数为行索引值,从0开始
+    } else{
+        alert("没有评论可以删除！");
+    }
+
+}
+
+function clearComment(){
+    var tComment = document.getElementById("commentTable");
+    var tbody = tComment.getElementsByTagName("tbody")[0];
+    if(tbody){
+        while(tbody.firstChild){
+            tbody.removeChild(tbody.firstChild);//removeChild()方法用于删除指定节点
+        }
+    }
+}
--- js/RealTime.js
+++ js/RealTime.js
@@ -1,4 +1,4 @@
-function updateTime(clock){
+function updateTime_1(){
     var now = new Date();
     var year = now.getFullYear();
     var month = now.getMonth() + 1;
@@ -9,8 +9,19 @@ function updateTime(clock){
     var day = now.getDay();
     var weeks = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
     var week = weeks[day];
-    var time = year + "年" + month + "月" + date + "日 " + "&nbsp"
+    var time = year + "年" + month + "月" + date + "日" + "&nbsp"
     + week + "&nbsp" + hour + ":" + minute + ":" + seconde;
-    clock.innerHTML = "当前时间： " + time;
+    return time;
 }
-window.setInterval("updateTime(clock)", 1000);
+
+function updateTime_2(){
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1;
+    var date = now.getDate();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var seconde = now.getSeconds();
+    var time = year + "." + month + "." + date +"  "+ hour + ":" + minute + ":" + seconde;
+    return time;
+}
